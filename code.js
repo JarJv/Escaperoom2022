6bf89ad53ac1b907cc8bb0e373f775a47a44fa75
JarJv <79714103+JarJv@users.noreply.github.com>
2022-09-16 10:19:05 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+var senha = ('81936')
+var verifi = prompt("Insira a senha para parar o cronômetro.")
+
+function remove_acento (verifi)
+{       
+    verifi = verifi.toLowerCase();                                                         
+    verifi = verifi.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    verifi = verifi.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    verifi = verifi.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    verifi = verifi.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    verifi = verifi.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    verifi = verifi.replace(new RegExp('[Ç]','gi'), 'c');
+    return verifi;                 
+}
+
+if (senha == remove_acento(this.verifi.toLowerCase())) {
+    window.location.href = "final.html"
+}
